80814477de731c35859910fdbf8110b4db6d4d9c
Pablo San-Jose <lekand@gmail.com>
2020-09-25 14:52:34 +0200

    fix #75

--- test/test_mesh.jl
+++ test/test_mesh.jl
@@ -2,5 +2,6 @@ using Quantica: nvertices, nedges, nsimplices
 
 @test begin
     mesh = buildmesh(marchingmesh((0,1), (0,2), points = (10, 20)))
-    nvertices(mesh) == 200 && nedges(mesh) == 541 && nsimplices(mesh) == 342
+    @test nvertices(mesh) == 200 && nedges(mesh) == 541 && nsimplices(mesh) == 342
+    @test_throws ArgumentError marchingmesh((SA[1,2], SA[2,3]))
 end
