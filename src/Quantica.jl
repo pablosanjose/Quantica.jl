dd825e07270adcbed0e28bf4174ad1e979677353
Pablo San-Jose <lekand@gmail.com>
2021-03-30 11:37:29 +0200

    adapt to new GLMakie

--- src/Quantica.jl
+++ src/Quantica.jl
@@ -6,7 +6,7 @@ module Quantica
 using Requires
 
 function __init__()
-      @require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("plot_makie.jl")
+      @require GLMakie = "e9467ef8-e4e7-5192-8a1a-b1aee30e663a" include("plot_makie.jl")
       @require VegaLite = "112f6efa-9a02-5b7d-90c0-432ed331239a" include("plot_vegalite.jl")
 end
 
