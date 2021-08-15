5c1e401066416cce5b8859d4d5763a3f57d33fdc
Pablo San-Jose <lekand@gmail.com>
2021-08-15 19:31:45 +0200

    wip

--- src/precompile.jl
+++ src/precompile.jl
@@ -12,8 +12,8 @@ for E in 0:2, L in 0:E
     for L´ in 0:L
         precompile(supercell, (Lattice{T,E,L}, Vararg{NTuple{L,Int},L´}))
     end
-    # precompile(nrange, (Int, Lattice{T,E,L}))
-    # precompile(site, (Lattice{T,E,L}, Int, SVector{L,Int}))
+    precompile(nrange, (Int, Lattice{T,E,L}))
+    precompile(site, (Lattice{T,E,L}, Int, SVector{L,Int}))
     # O = Complex{T}
     # Ms = (
     #     Quantica.HoppingTerm{Int64, Quantica.HopSelector{Missing, Missing, Missing, Missing, Quantica.NeighborRange}, Int64},
