5a01caa825e03dc2c3fdaebc0e42581fe38f2c01
Pablo San-Jose <lekand@gmail.com>
2022-01-01 19:03:22 +0100

    cleanup

--- src/eigensolver.jl
+++ src/eigensolver.jl
@@ -1,10 +1,10 @@
 ############################################################################################
 # Dynamic package loader
+#   This is in global Quantica scope to avoid name collisions between package and
+#   Eigensolvers.EigensolverBackend. We `import` instead of `using` to avoid collisions
+#   between several backends
 #region
 
-# This is in global Quantica scope to avoid name collisions between package and
-# Eigensolvers.EigensolverBackend. We `import` instead of `using` to avoid collisions
-# between several backends
 function ensureloaded(package::Symbol)
     if !isdefined(Quantica, package)
         @warn("Required package $package not loaded. Loading...")
@@ -13,16 +13,14 @@ function ensureloaded(package::Symbol)
     return nothing
 end
 
-# eigensolver(x...; kw...) = Eigensolvers.eigensolver(x...; kw...)
-
 #endregion
 
 ############################################################################################
 # Eigensolvers module
-# Strategy: combine a EigensolverBackend with a Hamiltonian (or Bloch) to produce an
-# Eigensolver, is essentially a FunctionWrapper from an SVector to a Spectrum === Eigen
-# An EigensolverBackend is defined by a set of kwargs for the eigensolver and a set of
-# methods AbstractMatrix -> Eigen associated to that EigensolverBackend
+#   Strategy: combine a EigensolverBackend with a Hamiltonian (or Bloch) to produce an
+#   Eigensolver, is essentially a FunctionWrapper from an SVector to a Spectrum === Eigen
+#   An EigensolverBackend is defined by a set of kwargs for the eigensolver and a set of
+#   methods AbstractMatrix -> Eigen associated to that EigensolverBackend
 #region
 
 module Eigensolvers
@@ -43,7 +41,8 @@ export eigensolver
 
 abstract type EigensolverBackend end
 
-(b::EigensolverBackend)(m) = throw(ArgumentError("The eigensolver backend $(typeof(b)) is not defined to work on $(typeof(m))"))
+(b::EigensolverBackend)(m) =
+    throw(ArgumentError("The eigensolver backend $(typeof(b)) is not defined to work on $(typeof(m))"))
 
 #### Arpack #####
 
@@ -148,45 +147,6 @@ Spectrum(evals::AbstractVector{<:Real}, evecs::AbstractMatrix) =
     Spectrum(complex.(evals), evecs)
 
 #endregion
-
-############################################################################################
-# ShiftInvert
-#region
-
-# function shiftinvert(matrix::SparseMatrixCSC{O}, origin)
-#     cols, rows = size(matrix)
-#     matrix´ = diagshift!(matrix, origin)
-#     F = factorize(matrix´)
-#     lmap = LinearMap{Tv}((x, y) -> ldiv!(x, F, y), cols, rows,
-#                          ismutating = true, ishermitian = false)
-#     return lmap
-# end
-
-# function diagshift!(matrix::SparseMatrixCSC, origin)
-#     vals = nonzeros(matrix)
-#     rowval = rowvals(matrix)
-#     for col in 1:size(matrix, 2)
-#         found_diagonal = false
-#         for ptr in nzrange(matrix, col)
-#             if col == rowval[ptr]
-#                 found_diagonal = true
-#                 vals[ptr] -= origin * I  # To respect non-scalar eltypes
-#                 break
-#             end
-#         end
-#         found_diagonal || throw(error("Sparse work matrix must include the diagonal."))
-#     end
-#     return matrix
-# end
-
-# function invertshift(ϵ::Vector{T}, origin) where {T}
-#     ϵ´ = similar(ϵ, real(T))
-#     ϵ´ .= real(inv.(ϵ) .+ (origin))  # Caution: we assume a real spectrum
-#     return ϵ´
-# end
-
-#endregion
-
 
 end # module
 
