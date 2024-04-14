9f266adbc98cdf8c978388deecd4b5ebf02a6646
Pablo San-Jose <lekand@gmail.com>
2024-04-14 08:36:41 +0200

    another try to reduce allocs

--- src/builders.jl
+++ src/builders.jl
@@ -387,25 +387,75 @@ function load_harmonics(f, ::Val{L}, ::Type{B}, norbs, ncells; atol) where {L,B}
     return hars
 end
 
-readline_types(f::IO, t) = only(readline_types(split(readline(f)), t))
-readline_types(f::IO, t1, t2, ts...) =
-    readline_types(split(readline(f)), t1, t2, ts...)
-readline_types(v::Vector, ts...) = _readline_types((), v, ts...)
+# readline_types(f::IO, t) = only(readline_types(split(readline(f)), t))
+# readline_types(f::IO, t1, t2, ts...) =
+#     readline_types(split(readline(f)), t1, t2, ts...)
+# readline_types(v::Vector, ts...) = _readline_types((), v, ts...)
 
-_readline_types(rs::Tuple, tokens, t, ts...) =
-    _readline_types((rs..., poptype!(tokens, t)), tokens, ts...)
-_readline_types(rs::Tuple, _) = rs
+# _readline_types(rs::Tuple, tokens, t, ts...) =
+#     _readline_types((rs..., poptype!(tokens, t)), tokens, ts...)
+# _readline_types(rs::Tuple, _) = rs
 
-poptype!(tokens, ::Type{T}) where {T<:Real} = parse(T, popfirst!(tokens))
+# poptype!(tokens, ::Type{T}) where {T<:Real} = parse(T, popfirst!(tokens))
 
-function poptype!(tokens, ::Type{C}) where {T,C<:Complex{T}}
-    r = parse(T, popfirst!(tokens))
-    i = parse(T, popfirst!(tokens))
-    return C(r,i)
+# function poptype!(tokens, ::Type{C}) where {T,C<:Complex{T}}
+#     r = parse(T, popfirst!(tokens))
+#     i = parse(T, popfirst!(tokens))
+#     return C(r,i)
+# end
+
+# poptype!(tokens, ::Type{S}) where {N,T,S<:SVector{N,T}} =
+#     S(ntuple(_ -> poptype!(tokens, T), Val(N)))
+
+readline_types(f::IO, t1) = only(line_types(readline(f), t1))
+readline_types(f::IO, t1, t2, ts...) = line_types(readline(f), t1, t2, ts...)
+
+function line_types(s::String, types...)
+    tokens = split(s)
+    realtypes = tupleflatten(realtype.(types)...)
+    reals = parse_reals(tokens, realtypes...)
+    vals = reals_to_types(reals, types...)
+    return vals
 end
 
-poptype!(tokens, ::Type{S}) where {N,T,S<:SVector{N,T}} =
-    S(ntuple(_ -> poptype!(tokens, T), Val(N)))
+realtype(t::Type{<:Real}) = t
+realtype(::Type{Complex{T}}) where {T} = (T, T)
+realtype(::Type{SVector{N,T}}) where {N,T<:Real} = ntuple(Returns(T), Val(N))
+realtype(::Type{SVector{N,Complex{T}}}) where {N,T<:Real} =
+    (t -> (t..., t...))(realtype(SVector{N,T}))
+
+parse_reals(tokens, realtypes::Vararg{<:Type,N}) where {N} =
+    ntuple(i -> parse(realtypes[i], tokens[i]), Val(N))
+
+function reals_to_types(reals, types::Vararg{<:Type,N}) where {N}
+    offset = Ref(0)
+    return ntuple(i -> real_to_type!(offset, reals, types[i]), Val(N))
+end
+
+real_to_type!(offset, reals, ::Type{T}) where {T<:Real} =
+    (o = offset[]; offset[] += 1; T(reals[o+1]))
+real_to_type!(offset, reals, ::Type{C}) where {C<:Complex} =
+    (o = offset[]; offset[] += 2; C(reals[o+1], reals[o+2]))
+real_to_type!(offset, reals, ::Type{S}) where {N,T,S<:SVector{N,T}} =
+    S(ntuple(_ -> real_to_type!(offset, reals, T), Val(N)))
+
+# reals_to_types(offset::Int, reals, ::Type{T}, ts...) where {T<:Real} =
+#     (T(reals[offset+1]), reals_to_types(offset+1, reals, ts...)...)
+# reals_to_types(offset, reals, ::Type{C}, ts...) where {C<:Complex} =
+#     (C(reals[offset+1], reals[offset+2]), reals_to_types(offset+2, reals, ts...)...)
+# reals_to_types(offset, reals, ::Type{S}, ts...) where {N,T,S<:SVector{N,T}} =
+#     (ntuple(i->C(reals[offset+1], reals[offset+2]), reals_to_types(offset+2, reals, ts...)...)
+# reals_to_types(offset, reals) = ()
+
+
+# reals_to_types(, ::Type{<:Real}, ts...) = (r, reals_to_types(x, ts...))
+# reals_to_types((r, x...), ::Type{<:Real}, ts...) = (r, reals_to_types(x, ts...))
+
+# realtype(t::Type{<:Real}) = t
+# realtype(::Type{Complex{T}}) where {T} = (T, T)
+# realtype(::Type{SVector{N,T}}) where {N,T<:Real} = ntuple(Returns(T), Val(N))
+# realtype(::Type{SVector{N,Complex{T}}}) where {N,T<:Real} =
+#     (t -> (t..., t...))(realtype(SVector{N,T}))
 
 push_if_nonzero!(ijv::IJV{<:Number}, (i, j, v), htol) =
     abs(v) > htol && push!(ijv, (i, j, v))
