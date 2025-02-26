cf94e3de21a65c3f26441b54ba3a90b320b0ccfa
Pablo San-Jose <lekand@gmail.com>
2025-02-26 17:57:38 +0100

    debugging test

--- test/test_greenfunction.jl
+++ test/test_greenfunction.jl
@@ -574,6 +574,7 @@ end
     g = LP.square() |> hopping(1) |> supercell(3,1) |> greenfunction(GS.Schur(; axis = 2, atol = 1e-2, callback))
     ρ = densitymatrix(g[])
     @test iszero(ref[])
+    @show diag(g(0)[])
     @test all(x->abs(real(x)) < 1e-6, diag(g(0)[]))
     @test !iszero(ref[])
     ref = Ref(0.0)
