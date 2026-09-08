6a24b50ddca018574a84ebc7fe87b1f70f4c0f24
pablosanjose <lekand@gmail.com>
2026-09-08 16:49:30 +0200

    WIP save

--- src/Quantica.jl
+++ src/Quantica.jl
@@ -67,6 +67,7 @@ include("greenfunction.jl")
 include("integrator.jl")
 include("observables.jl")
 include("meanfield.jl")
+include("scattering.jl")
 # Plumbing
 include("apply.jl")
 include("show.jl")
